b6d8a48dfec91fd9c9afeafd4bc2304e6d692e84
Ethan Jucovy <ethan.jucovy@gmail.com>
2017-04-30 18:38:56 -0400

    Paperwork

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
     pass
 
 setup(name='trac-WorkflowNotificationPlugin',
-      version='0.5',
+      version='0.6',
       description="Configurable notifications for trac tickets tied to workflow actions",
       long_description=long_description,
       packages=['workflow_notification'],
