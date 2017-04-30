dfc26c30e14e82b6a76d7c5de07e26078a81b42a
Ryan J Ollos <ryanollos@verasonics.com>
2017-04-30 16:09:55 -0700

    Refactor package data inclusion
    The templates were not included in development mode

--- setup.py
+++ setup.py
@@ -14,14 +14,19 @@ setup(name='trac-WorkflowNotificationPlugin',
       description="Configurable notifications for trac tickets tied to workflow actions",
       long_description=long_description,
       packages=['workflow_notification'],
+      package_data = {
+        'workflow_notification': [
+            'templates/*.html',
+            'templates/*.txt',
+        ],
+      },
       author='Ethan Jucovy',
       author_email='ejucovy@gmail.com',
-      url="http://trac-hacks.org/wiki/WorkflowNotificationPlugin",
+      url="https://trac-hacks.org/wiki/WorkflowNotificationPlugin",
       license='BSD',
       entry_points = {
         'trac.plugins':
             ['workflow_notification = workflow_notification']
         },
-      include_package_data=True,
       zip_safe=False,
       )
